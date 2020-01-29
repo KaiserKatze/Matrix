940116148124f1023f1ad75d69ed9995a80308de
KaiserKatze <donizyo@gmail.com>
2020-01-29 22:09:49 +0800

    Update Matrix.cpp

--- Matrix/Matrix.cpp
+++ Matrix/Matrix.cpp
@@ -18,78 +18,171 @@ int main()
     };
 
     std::cout << "m1 = " << std::endl
-        << m1.ToString() << std::endl;
+        << m1.ToString();
+
+    if (m1.GetData(0, 0) == 1
+        && m1.GetData(0, 1) == 0
+        && m1.GetData(0, 2) == 0
+        && m1.GetData(1, 0) == 0
+        && m1.GetData(1, 1) == 1
+        && m1.GetData(1, 2) == 0
+        && m1.GetData(2, 0) == 0
+        && m1.GetData(2, 1) == 0
+        && m1.GetData(2, 2) == 1)
+        std::cout << "Succeed" << std::endl;
+    else
+        std::cout << "Failed" << std::endl;
+
+    std::cout << std::endl;
+
+    //================================================
 
     Matrix<int, 3, 3> m2{
         1, 2, 3,
-        2, 4, 7,
-        3, 7, 14,
+        4, 5, 6,
+        7, 8, 9,
     };
 
     std::cout << "m2 = " << std::endl
-        << m2.ToString() << std::endl;
+        << m2.ToString();
+
+    if (m2.GetData(0, 0) == 1
+        && m2.GetData(0, 1) == 2
+        && m2.GetData(0, 2) == 3
+        && m2.GetData(1, 0) == 4
+        && m2.GetData(1, 1) == 5
+        && m2.GetData(1, 2) == 6
+        && m2.GetData(2, 0) == 7
+        && m2.GetData(2, 1) == 8
+        && m2.GetData(2, 2) == 9)
+        std::cout << "Succeed" << std::endl;
+    else
+        std::cout << "Failed" << std::endl;
+
+    std::cout << "m1 != m2 ";
+    if (m1 != m2)
+        std::cout << "Succeed" << std::endl;
+    else
+        std::cout << "Failed" << std::endl;
+    
+    std::cout << std::endl;
+
+    //================================================
 
     auto m3 = Matrix(m1);
 
     std::cout << "m3 = " << std::endl
-        << m3.ToString() << std::endl;
+        << m3.ToString();
+
+    if (m3.GetData(0, 0) == 1
+        && m3.GetData(0, 1) == 0
+        && m3.GetData(0, 2) == 0
+        && m3.GetData(1, 0) == 0
+        && m3.GetData(1, 1) == 1
+        && m3.GetData(1, 2) == 0
+        && m3.GetData(2, 0) == 0
+        && m3.GetData(2, 1) == 0
+        && m3.GetData(2, 2) == 1)
+        std::cout << "Succeed" << std::endl;
+    else
+        std::cout << "Failed" << std::endl;
+
+    std::cout << "m1 == m2 ";
+    if (m1 == m3)
+        std::cout << "Succeed" << std::endl;
+    else
+        std::cout << "Failed" << std::endl;
+
+    std::cout << std::endl;
+
+    //================================================
 
     auto m4 = m3.Transpose();
 
     std::cout << "m4 = " << std::endl
-        << m4.ToString() << std::endl;
+        << m4.ToString();
+
+    if (m4.GetData(0, 0) == 1
+        && m4.GetData(0, 1) == 0
+        && m4.GetData(0, 2) == 0
+        && m4.GetData(1, 0) == 0
+        && m4.GetData(1, 1) == 1
+        && m4.GetData(1, 2) == 0
+        && m4.GetData(2, 0) == 0
+        && m4.GetData(2, 1) == 0
+        && m4.GetData(2, 2) == 1)
+        std::cout << "Succeed" << std::endl;
+    else
+        std::cout << "Failed" << std::endl;
+
+    std::cout << std::endl;
+
+    //================================================
 
     IdentityMatrix<int, 4> m5;
 
     std::cout << "m5 = " << std::endl
         << m5.ToString() << std::endl;
 
+    //================================================
+
     IdentityMatrix<int, 6> m6;
 
     std::cout << "m6 = " << std::endl
         << m6.ToString() << std::endl;
+
+    //================================================
 
     m1 *= 40;
 
     std::cout << "m1 = " << std::endl
         << m1.ToString() << std::endl;
 
+    //================================================
+
     m1 /= 2;
 
     std::cout << "m1 = " << std::endl
         << m1.ToString() << std::endl;
+
+    //================================================
 
     m1 -= m2;
 
     std::cout << "m1 = " << std::endl
         << m1.ToString() << std::endl;
 
+    //================================================
+
     m2 += m1;
 
     std::cout << "m2 = " << std::endl
         << m2.ToString() << std::endl;
+
+    //================================================
 
     m2 = m1 * 3 + m2 / 2 - m1;
 
     std::cout << "m2 = " << std::endl
         << m2.ToString() << std::endl;
 
-    if (m4 == m3)
-    {
-        std::cout << "m4 == m3" << std::endl;
-    }
-    else
-    {
-        std::cout << "m4 != m3" << std::endl;
-    }
+    //================================================
 
     Scalar<int> s1 = 1;
 
+    //================================================
+
     MatrixQ<int, 3> m9 = s1 * m1 + m1 * s1;
+
+    //================================================
 
     int i1 = s1 * 2 + 2 * s1;
 
+    //================================================
+
     int i2 = s1.Transpose();
+
+    //================================================
 
     return 0;
 }
