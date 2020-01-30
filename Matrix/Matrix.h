8a0bf532f183e65000359130a97b954c6b87a6c5
KaiserKatze <donizyo@gmail.com>
2020-01-31 00:42:31 +0800

    Update Matrix.h

--- Matrix/Matrix.h
+++ Matrix/Matrix.h
@@ -636,6 +636,13 @@ operator*(const MatrixQ<_Ty, 4, order>& first, const MatrixQ<_Ty, 4, order>& sec
         return MatrixQ<_Ty, 4, order>{ c11, c21, c31, c41, c12, c22, c32, c42, c13, c23, c33, c43, c14, c24, c34, c44 };
 }
 
+// Vector de facto
+template <typename _Ty, int N, typename order>
+class MatrixMath::Matrix<_Ty, N, 1, order>
+    : public ProtoMatrix<_Ty, N, 1, order>
+{
+};
+
 // Scalar de facto
 template <typename _Ty, typename order>
 class MatrixMath::Matrix<_Ty, 1, 1, order>
