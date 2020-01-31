2f3a557ee9e3e89ad2d7c2f4673a5b1b648c183a
KaiserKatze <donizyo@gmail.com>
2020-01-31 22:27:04 +0800

    Update

--- Matrix/Matrix.cpp
+++ Matrix/Matrix.cpp
@@ -6,226 +6,28 @@
 #include "Matrix.h"
 #include "Geometry.h"
 
-using namespace MatrixMath;
-
 int main()
 {
-    std::cout << "Hello World!" << std::endl;
+    // Test: Scalar initialization
+    MatrixMath::Scalar<int> s1;
+    MatrixMath::Scalar<int> s2{ 100 };
+    MatrixMath::Scalar<int> s3(s2);
 
-    MatrixMath::Matrix<int, 3, 3> m1{
-        1, 0, 0,
-        0, 1, 0,
-        0, 0, 1,
-    };
+    std::cout
+        << "s1 -> " << s1 << std::endl
+        << "s2 -> " << s2 << std::endl
+        << "s3 -> " << s3 << std::endl;
 
-    std::cout << "m1 = " << std::endl
-        << m1.ToString();
+    // Test: Scalar user-defined conversion
+    int is1{ s2 };
 
-    if (m1.GetElement(0, 0) == 1
-        && m1.GetElement(0, 1) == 0
-        && m1.GetElement(0, 2) == 0
-        && m1.GetElement(1, 0) == 0
-        && m1.GetElement(1, 1) == 1
-        && m1.GetElement(1, 2) == 0
-        && m1.GetElement(2, 0) == 0
-        && m1.GetElement(2, 1) == 0
-        && m1.GetElement(2, 2) == 1)
-        std::cout << "Succeed" << std::endl;
-    else
-        std::cout << "Failed" << std::endl;
+    // Test: Scalar multiplication
+    s2 *= s2;
+    s3 *= 20;
 
-    std::cout << std::endl;
-
-    //================================================
-
-    MatrixMath::Matrix<int, 3, 3> m2{
-        1, 2, 3,
-        4, 5, 6,
-        7, 8, 9,
-    };
-
-    std::cout << "m2 = " << std::endl
-        << m2.ToString();
-
-    if (m2.GetElement(0, 0) == 1
-        && m2.GetElement(0, 1) == 2
-        && m2.GetElement(0, 2) == 3
-        && m2.GetElement(1, 0) == 4
-        && m2.GetElement(1, 1) == 5
-        && m2.GetElement(1, 2) == 6
-        && m2.GetElement(2, 0) == 7
-        && m2.GetElement(2, 1) == 8
-        && m2.GetElement(2, 2) == 9)
-        std::cout << "Succeed" << std::endl;
-    else
-        std::cout << "Failed" << std::endl;
-
-    std::cout << "m1 != m2 ";
-    if (m1 != m2)
-        std::cout << "Succeed" << std::endl;
-    else
-        std::cout << "Failed" << std::endl;
-    
-    std::cout << std::endl;
-
-    //================================================
-
-    auto m3 = MatrixMath::Matrix(m1);
-
-    std::cout << "m3 = " << std::endl
-        << m3.ToString();
-
-    if (m3.GetElement(0, 0) == 1
-        && m3.GetElement(0, 1) == 0
-        && m3.GetElement(0, 2) == 0
-        && m3.GetElement(1, 0) == 0
-        && m3.GetElement(1, 1) == 1
-        && m3.GetElement(1, 2) == 0
-        && m3.GetElement(2, 0) == 0
-        && m3.GetElement(2, 1) == 0
-        && m3.GetElement(2, 2) == 1)
-        std::cout << "Succeed" << std::endl;
-    else
-        std::cout << "Failed" << std::endl;
-
-    std::cout << "m1 == m2 ";
-    if (m1 == m3)
-        std::cout << "Succeed" << std::endl;
-    else
-        std::cout << "Failed" << std::endl;
-
-    std::cout << std::endl;
-
-    //================================================
-
-    auto m4 = m3.Transpose();
-
-    std::cout << "m4 = " << std::endl
-        << m4.ToString();
-
-    if (m4.GetElement(0, 0) == 1
-        && m4.GetElement(0, 1) == 0
-        && m4.GetElement(0, 2) == 0
-        && m4.GetElement(1, 0) == 0
-        && m4.GetElement(1, 1) == 1
-        && m4.GetElement(1, 2) == 0
-        && m4.GetElement(2, 0) == 0
-        && m4.GetElement(2, 1) == 0
-        && m4.GetElement(2, 2) == 1)
-        std::cout << "Succeed" << std::endl;
-    else
-        std::cout << "Failed" << std::endl;
-
-    std::cout << std::endl;
-
-    //================================================
-
-    IdentityMatrix<int, 4> m5;
-
-    std::cout << "m5 = " << std::endl
-        << m5.ToString() << std::endl;
-
-    //================================================
-
-    IdentityMatrix<int, 6> m6;
-
-    std::cout << "m6 = " << std::endl
-        << m6.ToString() << std::endl;
-
-    //================================================
-
-    m1 *= 40;
-
-    std::cout << "m1 = " << std::endl
-        << m1.ToString() << std::endl;
-
-    //================================================
-
-    m1 /= 2;
-
-    std::cout << "m1 = " << std::endl
-        << m1.ToString() << std::endl;
-
-    //================================================
-
-    m1 -= m2;
-
-    std::cout << "m1 = " << std::endl
-        << m1.ToString() << std::endl;
-
-    //================================================
-
-    m2 += m1;
-
-    std::cout << "m2 = " << std::endl
-        << m2.ToString() << std::endl;
-
-    //================================================
-
-    m2 = m1 * 3 + m2 / 2 - m1;
-
-    std::cout << "m2 = " << std::endl
-        << m2.ToString() << std::endl;
-
-    //================================================
-
-    Scalar<int> s1 = 1;
-
-    //================================================
-
-    MatrixMath::MatrixQ<int, 3> m9 = s1 * m1 + m1 * s1;
-
-    //================================================
-
-    int i1 = s1 * 2 + 2 * s1;
-
-    std::cout << "i1 = " << i1 << std::endl;
-    if (i1 == 4)
-        std::cout << "Succeed" << std::endl;
-    else
-        std::cout << "Failed" << std::endl;
-    std::cout << std::endl;
-
-    //================================================
-
-    s1 = 100;
-
-    std::cout << "s1 = " << s1 << std::endl;
-    if (s1 == 100)
-        std::cout << "Succeed" << std::endl;
-    else
-        std::cout << "Failed" << std::endl;
-    std::cout << std::endl;
-
-    //================================================
-
-    int i2 = s1.Transpose();
-
-    std::cout << "i2 = " << i2 << std::endl;
-    if (i2 == 100)
-        std::cout << "Succeed" << std::endl;
-    else
-        std::cout << "Failed" << std::endl;
-    std::cout << std::endl;
-
-    //================================================
-
-    Scalar<int> s2{ 100 };
-    int i3 = s2;
-
-    std::cout << "i3 = " << i3 << std::endl;
-    if (i3 == 100)
-        std::cout << "Succeed" << std::endl;
-    else
-        std::cout << "Failed" << std::endl;
-    std::cout << std::endl;
-
-    //================================================
-
-    MatrixMath::Matrix4f<> m4f{ IdentityMatrix<float, 4>() };
-
-    //================================================
-
+    std::cout
+        << "s2 = (s2 * s2) -> " << s2 << std::endl
+        << "s3 = (s3 * 20) -> " << s3 << std::endl;
 
     return 0;
 }
