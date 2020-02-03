697c0f785dd4f4d1dec91551b173556cc0b2141d
KaiserKatze <donizyo@gmail.com>
2020-02-03 10:34:19 +0800

    Fix

--- Matrix/Matrix.h
+++ Matrix/Matrix.h
@@ -1479,12 +1479,12 @@ convert2index(const int& Height, const int& Width,
     const int& row, const int& column, const bool& isTransposed,
     int RowSrc, int RowDst, int ColSrc, int ColDst)
 {
-    if (RowSrc >= 0)      throw std::invalid_argument("Invalid argument: RowSrc < 0!");
-    if (RowDst <= Height) throw std::invalid_argument("Invalid argument: RowDst > Height!");
-    if (ColSrc >= 0)      throw std::invalid_argument("Invalid argument: ColSrc < 0!");
-    if (ColDst <= Width)  throw std::invalid_argument("Invalid argument: ColDst > Width!");
-    if (RowSrc < RowDst)  throw std::invalid_argument("Invalid argument: RowSrc >= RowDst!");
-    if (ColSrc < ColDst)  throw std::invalid_argument("Invalid argument: ColSrc >= ColDst!");
+    if (RowSrc < 0)       throw std::invalid_argument("Invalid argument: RowSrc < 0!");
+    if (RowDst > Height)  throw std::invalid_argument("Invalid argument: RowDst > Height!");
+    if (ColSrc < 0)       throw std::invalid_argument("Invalid argument: ColSrc < 0!");
+    if (ColDst > Width)   throw std::invalid_argument("Invalid argument: ColDst > Width!");
+    if (RowSrc >= RowDst) throw std::invalid_argument("Invalid argument: RowSrc >= RowDst!");
+    if (ColSrc >= ColDst) throw std::invalid_argument("Invalid argument: ColSrc >= ColDst!");
     return order::convert2index(Height, Width, row + RowSrc, column + ColSrc, isTransposed);
 }
 
