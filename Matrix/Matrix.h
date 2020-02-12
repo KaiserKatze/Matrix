31c7ff819e8d1959d393f70674405399473288bb
KaiserKatze <donizyo@gmail.com>
2020-02-12 14:49:45 +0800

    Update

--- Matrix/Matrix.h
+++ Matrix/Matrix.h
@@ -1994,7 +1994,10 @@ IsInvertible(const MatrixType& matrix)
     // for long long, float, and double;
     // what if ElementType is an instance of Matrix?
     // what if ElementType is an instance of std::complex?
-    return MetaMath::Zero<_Ty> != Determinant(matrix).value();
+    using namespace MetaMath;
+    // extend the definition of Zero<_Ty> by
+    // using more namespaces here
+    return Zero<_Ty> != Determinant(matrix).value();
 }
 
 template <typename MatrixType>
